f49df95431fae08dc742c7e9464d86c45a3e8f9e
olivertembo <tembokoliver2@yahoo.com>
2021-09-17 16:34:51 +0300

    first commit

---
+++ index.ts
@@ -0,0 +1,21 @@
+const api = "httpa://api.abc/data";
+
+interface Data {
+    name: String
+    age: number
+}
+
+
+function getData(): Promise<Data[]> {
+    return (
+        fetch(api)
+        .then( resource => resource.json())
+        .then(resource =>{
+            return resource as Data[];
+        })
+    );
+}
+
+const data = getData
+
+
